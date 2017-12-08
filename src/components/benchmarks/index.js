75fe6814f17b39cdaadda215f9235620e3f48a73
Bruno Lemos <brunohplemos@gmail.com>
2017-12-08 14:31:56 -0200

    Reenable glamorous-native benchmark
    Possible now that detox+jest+react-native integration was fixed

--- src/components/benchmarks/index.js
+++ src/components/benchmarks/index.js
@@ -1,9 +1,9 @@
 // all benchmarks
 import FelaInlineTable from './fela/inline'
 import FelaSimpleTable from './fela/simple'
-// import GlamorousInlineTable from './glamorous/inline'
-// import GlamorousPropsTable from './glamorous/props'
-// import GlamorousSimpleTable from './glamorous/simple'
+import GlamorousInlineTable from './glamorous/inline'
+import GlamorousPropsTable from './glamorous/props'
+import GlamorousSimpleTable from './glamorous/simple'
 import ReactNativeInlineOnlyTable from './react-native/inline-only'
 import ReactNativeStyleSheetTable from './react-native/stylesheet'
 import StyledComponentsDecoupledCellTable from './styled-components/decoupled-cell'
@@ -13,9 +13,9 @@ import StyledComponentsSimpleTable from './styled-components/simple'
 export const benchmarks = [
   FelaInlineTable,
   FelaSimpleTable,
-  // GlamorousInlineTable,
-  // GlamorousPropsTable,
-  // GlamorousSimpleTable,
+  GlamorousInlineTable,
+  GlamorousPropsTable,
+  GlamorousSimpleTable,
   ReactNativeInlineOnlyTable,
   ReactNativeStyleSheetTable,
   StyledComponentsDecoupledCellTable,
